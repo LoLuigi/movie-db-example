da0bc0fa85e6496b0413aef0aa67946ad279ad64
LoLuigi <171548877+LoLuigi@users.noreply.github.com>
2024-06-04 15:52:23 +0200

    Added Categorys

--- frontend/src/components/Mycomp/index.jsx
+++ frontend/src/components/Mycomp/index.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 //import '../values/colors.js'
 import './styles.css'
+import Picture from '../Picture';
 
 
-const Mycomp = (props) =>(
+
+const Mycomp = ({movie1}) =>
+    (
+    // console.log(movie1)
     <li className="movie-db-props-children">
-        {props.children}
+        <div>
+              <Link to={`movies/${movie1.Id}`}>
+              <Picture src={movie1.Poster}/>
+              </Link>
+              </div>
+                <p className="p1">{movie1.Title} ({movie1.Year.substring(0, movie1.Year.length - 2)})</p>
+                <p className='p2'>{movie1.Director}</p>
     </li>
 );
 
--- frontend/src/pages/movies.jsx
+++ frontend/src/pages/movies.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';   
 import { Link } from 'react-router-dom';
 import { useLoaderData } from 'react-router-dom';
 
@@ -35,35 +36,50 @@ export async function loader() {
     };
   }, {});
 
-  console.log(moviesById);
-
+  // console.log(moviesById);
+  // console.log(categorys)
   return { movies,moviesById,categorys };
+  
 }
 
 const MoviesPage = () => {
   const { movies } = useLoaderData();
-  console.log({ movies });
+  // console.log({movies});
+  const {categorys} = useLoaderData();
+  // let categorysList = Object.keys(categorys);
+  const {moviesById} = useLoaderData();
+
   // to get an element from list: list.find()
   // to use map on object, convert to to list first: Object.values()
   // list.length gives number of items
   return (
     <Page title="Movies">
-      <List>
-        {
-          movies.map((movie) => (
-            <Mycomp key={`movie-list-item-${movie.Id}`}>            
-            <div>
-            <Link to={`movies/${movie.Id}`}>
-            <Picture src={movie.Poster}/>
-            </Link>
-            </div>
-              <p className="p1">{movie.Title} ({movie.Year.substring(0, movie.Year.length - 2)})</p>
-              <p className='p2'>{movie.Director}</p>          
-            </Mycomp>
-          ))
-        }
-      </List>
-    </Page>
+      {
+      Object.entries(categorys).map(([categoryTitle, categorysList]) => (
+        <Page title = {`${categoryTitle} (${categorysList.length})`}>
+        {categorysList.map((_Id) => {
+        const movie = moviesById[_Id]
+        })}
+        <List>
+          {
+            categorysList.map((_Id) => (                            
+              <Mycomp movie1={moviesById[_Id]} key={`movie-list-item-${_Id}`}>            
+              {/* <div>
+              <Link to={`movies/${movie.Id}`}>
+              <Picture src={movie.Poster}/>
+              </Link>
+              </div>
+                <p className="p1">{movie.Title} ({movie.Year.substring(0, movie.Year.length - 2)})</p>
+                <p className='p2'>{movie.Director}</p>           */}
+              </Mycomp>
+            
+          ))}
+        </List>
+
+      </Page>
+      ))
+    };
+      </Page>
   );
 };
 
